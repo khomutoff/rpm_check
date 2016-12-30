6f213e33d4eae7a86cd1bdb29dc4db75427bebdd
Dmitriy Khomutov <khomutoff@gmail.com>
2016-12-29 19:59:49 -0800

    Module documentation

--- rpm_check.py
+++ rpm_check.py
@@ -23,6 +23,32 @@ along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
 import rpm
 
+DOCUMENTATION = '''
+---
+module: rpm_check
+short_description: Check installed rpm packages
+version_added: "1.9"
+description:
+  - Checks if rpm package is installed
+options:
+  name:
+    description:
+      - RPM package name. To operate on several packages this can accept a comma separated list of packages or (as of 2.0) a list of packages."
+    required: true
+    default: null
+  state:
+    description:
+      - Currently supports only one state of a package (C(installed)).
+    required: false
+    choices: [ "installed" ]
+    default: "installed"
+
+# informational: requirements for nodes
+requirements: [ rpm ]
+author:
+  - "Dmitriy Khomutov"
+'''
+
 EXAMPLES = '''
 - name: Check if Apache installed
   rpm_check:
@@ -36,12 +62,16 @@ EXAMPLES = '''
 
 def is_installed(packages):
     rpmdb_ts = rpm.TransactionSet()
+    existing_packages = []
     missing_packages = []
     for p in packages:
         installed_pkgs = rpmdb_ts.dbMatch('name', p)
         if not installed_pkgs:
             missing_packages.append(p)
-    return missing_packages
+        else:
+            pkg = installed_pkgs.next()
+            existing_packages.append("%s-%s-%s-%s" % (pkg['name'], pkg['version'], pkg['release'], pkg['arch']))
+    return existing_packages, missing_packages
 
 def check_rpms(module, packages, state):
     result = {}
@@ -50,9 +80,11 @@ def check_rpms(module, packages, state):
     result['changed'] = False
     result['rc'] = 0
     if state == 'installed':
-        missing_pkg_names_str = ", ".join(is_installed(packages))
-        if missing_pkg_names_str:
-            module.fail_json(msg="No RPMs matching '%s' found on system" % missing_pkg_names_str)
+        existing_pkg_names, missing_pkg_names = is_installed(packages)
+        if missing_pkg_names:
+            module.fail_json(msg="No RPMs matching '%s' found on system" % ", ".join(missing_pkg_names))
+        if existing_pkg_names:
+            result['results'] = existing_pkg_names
     return result
 
 def main():
